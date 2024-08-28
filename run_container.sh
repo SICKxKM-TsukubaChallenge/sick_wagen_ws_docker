ee4760b5e391416c13ec6cba631b5512be28806d
N-Iemura <iemura-n1207@keio.jp>
2024-08-28 16:42:30 +0900

    modify shell_script for SICK PC

--- run_container.sh
+++ run_container.sh
@@ -2,10 +2,10 @@
 
 # Define the common options
 VOLUME_OPTS_MINIMUM="--volume $(pwd):/root/sick_wagen_ws_docker"
-VOLUME_OPTS="--volume $(pwd):/root/sick/sick_wagen_ws_docker --group-add dialout --volume /dev/tty* --volume /dev/video* --volume /dev/fdcanusb"
+VOLUME_OPTS="--volume $(pwd):/root/sick_wagen_ws_docker --group-add dialout --volume /dev/tty* --volume /dev/video* --volume /dev/fdcanusb"
 IMAGE_NAME="sick/ros:noetic"
 DOCKER_CMD="docker run --rm -it"
-ROCKER_CMD="rocker --x11 --nvidia --user --network=host --privileged $VOLUME_OPTS $IMAGE_NAME"
+ROCKER_CMD="rocker $IMAGE_NAME --x11 --nvidia --user --network=host --privileged $VOLUME_OPTS"
 
 # Check if -d option is passed
 if [ "$1" == "-d" ]; then
